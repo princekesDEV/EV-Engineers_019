65581f84bd1e914f64fc1d8cfc34ac4fb4347f78
princekesDEV <kesprince1250@gmail.com>
2024-10-02 14:32:15 +0530

    Side Bar Layout Complted

---
+++ products-sidebar.js
@@ -0,0 +1,6 @@
+function relevance() {
+  let dropdown = document.querySelector("#relevance");
+  let content = document.querySelector("#relevance-content");
+
+  dropdown.classList.toggle("active");
+}
